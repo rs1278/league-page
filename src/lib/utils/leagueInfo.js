882315c445ea00067238eb5e45df616ba024e17f
rs1278 <92549755+rs1278@users.noreply.github.com>
2024-10-31 15:21:22 -0600

    Update leagueInfo.js
    Updated for 2024 Season

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "992466890507239424"; // your league ID
+export const leagueID = "1124816105748037632"; // your league ID
 export const leagueName = "THE League"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
